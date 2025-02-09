7fd683eda32fdc7c4031affbde7f4d382cafb564
zain-here777 <zainii.cs@gmail.com>
2025-02-09 16:31:14 +0500

    latest

--- dom.js
+++ dom.js
@@ -1,20 +1,51 @@
-// getElementById //
+// // getElementById //
+//
+// const fiza = document.getElementById('para');
+//
+// fiza.innerHTML = "Javascript is saying Hi to you!";
+//
+// // getElementByClassName //
+//
+// const classSelector = document.getElementsByClassName('para-2');
+// console.log(classSelector)
+// classSelector[1].innerHTML = 'This is class selector'
+//
+//
+// // querySelector (will pick only the first matched class)  //
+//
+// const query = document.querySelector('.para-2');
+// console.log(query)
+//
+// query.innerHTML = 'This is Query Selector';
 
-const fiza = document.getElementById('para');
+function showHidePara() {
+    const para = document.getElementById('para-show');
 
-fiza.innerHTML = "Javascript is saying Hi to you!";
+    if (para.style.display === 'none') {
+        para.style.display = 'block';
+    } else {
+        para.style.display = 'none';
+    }
+}
 
-// getElementByClassName //
+const changeBtnText = () => {
+    const clickBtn = document.getElementById('clickBtn');
 
-const classSelector = document.getElementsByClassName('para-2');
-console.log(classSelector)
-classSelector[1].innerHTML = 'This is class selector'
+    if (clickBtn.innerHTML === "Clicked"){
+        clickBtn.innerHTML = "Click me";
+    }else{
+        clickBtn.innerHTML = "Clicked"
+    }
+}
 
+function changeBoxColor() {
+    const box = document.getElementById('colorBox');
 
-// querySelector (will pick only the first matched class)  //
-
-const query = document.querySelector('.para-2');
-console.log(query)
-
-query.innerHTML = 'This is Query Selector';
-
+    if (box.style.backgroundColor === 'lightgray') {
+        box.style.backgroundColor = 'skyBlue';
+        box.innerHTML = 'This is the Blue color Box';
+    } else {
+        box.style.backgroundColor = 'lightgray';
+        box.innerHTML = 'This is the Gray color Box';
+    }
+}
