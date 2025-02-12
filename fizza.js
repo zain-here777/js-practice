42a21bb6ebf12cfd6aea635c93a25fbd3f6e82a5
Fizza-Khan-801 <fizzaialikhan@gmail.com>
2025-02-12 19:19:45 +0500

    add

--- fizza.js
+++ fizza.js
@@ -1,10 +1,53 @@
-const head = document.getElementById('intro');
-head.innerHTML = "say hello";
+// const head = document.getElementById('intro');
+// head.innerHTML = "say hello";
 
-const name = document.getElementsByClassName('self');
-console.log(name)
-name[1].innerHTML = 'u can call me fizza';
+// const name = document.getElementsByClassName('self');
+// console.log(name)
+// name[1].innerHTML = 'u can call me fizza';
 
-const specific = document.querySelector('.self');
-console.log(specific)
-specific[3].innerHTML = 'change it';
+// const specific = document.querySelector('.self');
+// console.log(specific)
+// specific.innerHTML = 'change it';
+
+
+// on click event
+
+let votes = 0;
+        const vote = (action) => {
+            if (action === "up") {
+                votes++;
+            } else if (action === "down") {
+                votes--;
+            }
+            document.getElementById("voteCount").innerHTML = "Votes: " + votes;
+        }
+
+
+// ondbl click
+
+const changeclr = () => {
+    const btn = document.getElementById('btn');
+    if(
+        btn.style.color === 'red'
+    ){
+        btn.style.color = 'blue';
+    }
+    else{
+        btn.style.color = 'red';
+    }
+}
+
+// on key press
+
+const keypress = () => {
+    const box = document.getElementById('box');
+    if (
+        box.style.backgroundcolor === 'yellow'
+    )
+    {
+        box.style.backgroundcolor = 'green';
+    }
+    else{
+        box.style.backgroundcolor = 'yellow';
+    }
+}   
