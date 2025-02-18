007f953476e0fcc6d309b6418da0caf218d0f9ca
Fizza-Khan-801 <fizzaialikhan@gmail.com>
2025-02-18 22:07:18 +0500

    add

--- fizza.js
+++ fizza.js
@@ -63,110 +63,126 @@
 
 // form events
 
-document.getElementById('form') .addEventListener("submit" , function(){
-    alert('form submitted');
-});
-document.getElementById('name') .addEventListener("input" , function(){
-    console.log('input');
-});
-document.getElementById('name') .addEventListener("change" , function(){
-    const self = document.getElementById('name');
-    self.style.backgroundColor = 'lightblue';
-});
-document.getElementById('name') .addEventListener("focus" , function(){
-    const self = document.getElementById('name');
-    self.style.backgroundColor = 'lightgray';
-});
-document.getElementById('name') .addEventListener("blur" , function(){
-    const self = document.getElementById('name');
-    self.style.backgroundColor = 'white';
-});
-document.getElementById('mail') .addEventListener("input" , function(){
-    console.log('input');
-});
-document.getElementById('mail') .addEventListener("change" , function(){
-    const self = document.getElementById('mail');
-    self.style.backgroundColor = 'lightblue';
-});
-document.getElementById('mail') .addEventListener("focus" , function(){
-    const self = document.getElementById('mail');
-    self.style.backgroundColor = 'lightgray';
-});
-document.getElementById('mail') .addEventListener("blur" , function(){
-    const self = document.getElementById('mail');
-    self.style.backgroundColor = 'white';
-});
-document.getElementById('password') .addEventListener("input" , function(){
-    console.log('input');
-});
-document.getElementById('password') .addEventListener("change" , function(){
-    const self = document.getElementById('password');
-    self.style.backgroundColor = 'lightblue';
-});
-document.getElementById('password') .addEventListener("focus" , function(){
-    const pass = document.getElementById('password');
-    pass.style.backgroundColor = 'lightgray';
-});
-document.getElementById('password') .addEventListener("blur" , function(){
-    const pass = document.getElementById('password');
-    pass.style.backgroundColor = 'white';
-});
+// document.getElementById('form') .addEventListener("submit" , function(){
+//     alert('form submitted');
+// });
+// document.getElementById('name') .addEventListener("input" , function(){
+//     console.log('input');
+// });
+// document.getElementById('name') .addEventListener("change" , function(){
+//     const self = document.getElementById('name');
+//     self.style.backgroundColor = 'lightblue';
+// });
+// document.getElementById('name') .addEventListener("focus" , function(){
+//     const self = document.getElementById('name');
+//     self.style.backgroundColor = 'lightgray';
+// });
+// document.getElementById('name') .addEventListener("blur" , function(){
+//     const self = document.getElementById('name');
+//     self.style.backgroundColor = 'white';
+// });
+// document.getElementById('mail') .addEventListener("input" , function(){
+//     console.log('input');
+// });
+// document.getElementById('mail') .addEventListener("change" , function(){
+//     const self = document.getElementById('mail');
+//     self.style.backgroundColor = 'lightblue';
+// });
+// document.getElementById('mail') .addEventListener("focus" , function(){
+//     const self = document.getElementById('mail');
+//     self.style.backgroundColor = 'lightgray';
+// });
+// document.getElementById('mail') .addEventListener("blur" , function(){
+//     const self = document.getElementById('mail');
+//     self.style.backgroundColor = 'white';
+// });
+// document.getElementById('password') .addEventListener("input" , function(){
+//     console.log('input');
+// });
+// document.getElementById('password') .addEventListener("change" , function(){
+//     const self = document.getElementById('password');
+//     self.style.backgroundColor = 'lightblue';
+// });
+// document.getElementById('password') .addEventListener("focus" , function(){
+//     const pass = document.getElementById('password');
+//     pass.style.backgroundColor = 'lightgray';
+// });
+// document.getElementById('password') .addEventListener("blur" , function(){
+//     const pass = document.getElementById('password');
+//     pass.style.backgroundColor = 'white';
+// });
 
 
-//objects
+// //objects
 
-function merge(obj1 , obj2){
-    return Object.assign({} , obj1 , obj2);
+// function merge(obj1 , obj2){
+//     return Object.assign({} , obj1 , obj2);
+// }
+// const obj1 = {
+//     name : 'fizza',
+//     age : 21,
+// };
+// const obj2 = {
+//     color : 'blue',
+//     height : 5.6,
+// };
+// console.log(merge(obj1 , obj2));
+
+
+// const book = {
+//     title : 'jannat kay pattay',
+//     author : 'Nemrah Ahmed',
+//     year : 2012,
+// };
+// Object.values(book).forEach(value => {
+//     console.log(value);
+// });
+
+
+// //events
+
+// // resize
+
+// window.addEventListener("resize" , function(){
+// if (window.innerWidth < 768) {
+//     console.log ('this is for tablets');
+// } else{
+//     console.log ('this is for laptops');
+// }
+// });
+
+// window.addEventListener("resize", function() {
+//     if (window.innerWidth < 786) {
+//         document.body.style.background = "lightblue"; 
+//     } else {
+//         document.body.style.background = "lightgray"; 
+//     }
+// });
+
+
+// // on load
+
+// window.onload = function() {
+//     document.getElementById("loader").style.display = "none";
+//     document.getElementById("content").style.display = "block";
+// };
+
+//calculator
+
+function appendValue(value) {
+    document.getElementById('display').value += value;
 }
-const obj1 = {
-    name : 'fizza',
-    age : 21,
-};
-const obj2 = {
-    color : 'blue',
-    height : 5.6,
-};
-console.log(merge(obj1 , obj2));
 
-
-const book = {
-    title : 'jannat kay pattay',
-    author : 'Nemrah Ahmed',
-    year : 2012,
-};
-Object.values(book).forEach(value => {
-    console.log(value);
-});
-
-
-//events
-
-// resize
-
-window.addEventListener("resize" , function(){
-if (window.innerWidth < 768) {
-    console.log ('this is for tablets');
-} else{
-    console.log ('this is for laptops');
+function clearDisplay() {
+    document.getElementById('display').value = '';
 }
-});
 
-window.addEventListener("resize", function() {
-    if (window.innerWidth < 786) {
-        document.body.style.background = "lightblue"; 
-    } else {
-        document.body.style.background = "lightgray"; 
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (e) {
+        alert('Invalid Expression');
     }
-});
-
-
-// on load
-
-window.onload = function() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("content").style.display = "block";
-};
-
-// on scroll
+}
 
 
